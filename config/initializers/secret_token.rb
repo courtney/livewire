c80ba949eb3b36078e0ce8a3885771d3b30ad654
courtney <courtney@carbonfive.com>
2011-04-20 17:02:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foragesf::Application.config.secret_token = '69d4fba85fc4c0af1b50210490801989db50d73151c8095af2ae3619c610cae4044814da060fb0fb13663bd1ff50892837d8a2d44d0983e89ca558a004e54434'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Foragesf::Application.config.session_store :cookie_store, :key => '_foragesf_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Foragesf::Application.config.session_store :active_record_store
