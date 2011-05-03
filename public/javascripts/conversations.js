38a27e0683c2e09d9b78bbb8cccaa7705bb56c64
courtney <courtney@carbonfive.com>
2011-05-03 16:04:13 -0700

    scrolling, cleanup and adding left nav

--- public/javascripts/conversations.js
+++ public/javascripts/conversations.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	getTopics();	
 	getConversations();	
 	$('#message').submit(postMessage);
 });
@@ -6,29 +7,29 @@ $(document).ready(function() {
 function postMessage(data) {
 		var msg = $(data.target).find('input:first').val();
 		$.post("/message", { message: msg } );
-		$('#twitter ul').append('<li><div>' + msg + '</div></li>');			
-		// getConversations();		
+		$('#twitter ul').append('<li><div>' + msg  + '<em>chemphill <span>8 days ago</span></em></div></li>');	
+		$('#twitter').scrollTop($('#twitter ul').height());		
 		return false;
 }
 
 function getConversations() {
-	$('#twitter').append('<ul>');
-		// $.getJSON("/topics", function(json) {
-		// var html = "";
-		// var topics = [];
-		// 
-		// $(json.topics).each(function(groups) {
-		// 	$('#twitter').append('<ul><li><h2>' + this.name + ' : ' + this.creator.username + ' : '+ this.id+'</h2></li></ul>');
-			$.getJSON("/topics/21084", function(data) {
-				console.log(data);
-				$(data.messages).each(function(groups) {
-					$('#twitter ul').append('<li><div>' + this.message + '</div></li>');
-				});
-			});										
-		// });					
-	// });				
+	$('#twitter').append('<ul>');		
+		$.getJSON("/topics/21084", function(data) {
+			$(data.messages).each(function(groups) {
+				$('#twitter ul').append('<li><div>' + this.message + '<em>chemphill <span>8 days ago</span></em></div></li>');
+			});
+		});												
 }
 
+function getTopics() {
+	$.getJSON("/topics", function(json) {
+	var html = "";
+	var topics = [];	
+		$(json.topics).each(function(groups) {
+			$('#nav').append('<li id="'+this.id+'"><h2>' + this.name + '</h2></li>');			
+		});					
+	});
+}
 
 // $("#sidebar").ajaxError(function(e, xhr, settings, exception) {
 // 	$(this).text( 'error in: ' + settings.url + ' \n'+'error:\n' + xhr.responseText );
